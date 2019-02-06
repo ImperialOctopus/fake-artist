271364d47b09ce405873c8be74767fb86b4dfe01
Zoey Child <imperialoctopus@gmail.com>
2019-02-06 14:55:58 +0000

    Install vue

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
-
 // Webpack CSS import
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 
-// JS import
-import Vue from 'vue';
-import VueOnsen from 'vue-onsenui'; // This imports 'onsenui', so no need to import it separately
+import Vue from 'vue'
+import VueOnsen from 'vue-onsenui';
+import App from './App.vue'
 
+Vue.config.productionTip = false
 Vue.use(VueOnsen); // VueOnsen set here as plugin to VUE. Done automatically if a call to window.Vue exists in the startup code.
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
