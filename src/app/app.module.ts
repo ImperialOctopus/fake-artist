925b953e5f07743b3c5026624963406a016e956a
Zoey Child <imperialoctopus@gmail.com>
2019-03-27 22:00:05 +0000

    Update container css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { MenuComponent } from "./menu/menu.component";
-import { PlayComponent } from "./play/play.component";
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PlayComponent } from './play/play.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
-  { path: "", component: MenuComponent },
-  { path: "play/:n", component: PlayComponent },
-  { path: "**", component: MenuComponent }
+  { path: '', component: MenuComponent },
+  { path: 'play/:n', component: PlayComponent },
+  { path: '**', component: MenuComponent }
 ];
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, PlayComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }), BrowserAnimationsModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), ServiceWorkerModule.register('ngsw-worker.js',
+    { enabled: environment.production }), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
