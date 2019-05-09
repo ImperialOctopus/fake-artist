3b7bb140f5c20f2620ab75d58b4bd94e6b00972d
Zoey Child <imperialoctopus@gmail.com>
2019-05-09 23:23:22 +0100

    Remove form states from menu page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { WordlistService } from '../wordlist.service';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,18 +11,15 @@ import { Router } from '@angular/router';
 export class MenuComponent implements OnInit {
   playerNumber: number;
   roomName: string;
-  formState: number;
-
-  online: boolean;
 
   constructor(
+    private wordlistService: WordlistService,
+    private database: DatabaseService,
     private router: Router
   ) { }
 
   ngOnInit() {
     this.playerNumber = 3;
-    this.online = false;
-    this.formState = 0;
     this.roomName = '';
   }
 
@@ -36,28 +35,44 @@ export class MenuComponent implements OnInit {
   }
 
   playOffline() {
+    const r = 'Offline';
+    const f = Math.floor(Math.random() * this.playerNumber) + 1;
+    const p = this.wordlistService.generatePrompt();
+
     this.router.navigate(['/play', { n: this.playerNumber }]);
   }
+  playOnlineCreate() {
+    const r = this.roomName;
+    const f = Math.floor(Math.random() * this.playerNumber) + 1;
+    const p = this.wordlistService.generatePrompt();
 
-  join() {
-    this.router.navigate(['/online', { host: '0', name: this.roomName }]);
-  }
-  hostSettings() {
-    this.formState = 2;
-  }
+    this.database.create(
+      this.roomName,
+      Math.floor(Math.random() * this.playerNumber) + 1,
+      this.playerNumber,
+      this.wordlistService.generatePrompt());
 
-  hostCancel() {
-    this.formState = 1;
+    this.router.navigate(['/play', { name: this.roomName, n: this.playerNumber }]);
   }
-  host() {
-    this.router.navigate(['/online', { host: '1', name: this.roomName, n: this.playerNumber }]);
-  }
+  playOnlineJoin() {
+    const r = this.roomName;
+    const f = Math.floor(Math.random() * this.playerNumber) + 1;
+    const p = this.wordlistService.generatePrompt();
 
-  switchMode() {
-    if (this.formState === 0) {
-      this.formState = 1;
-    } else if (this.formState === 1) {
-      this.formState = 0;
-    }
+    this.database.getRoomInfo(this.roomName).then((roomInfo) => {
+      console.log(roomInfo);
+      this.prompt = roomInfo[0];
+      this.fake = roomInfo[1];
+      this.playerNumber = roomInfo[2];
+      this.database.join(this.roomName, this.playerNumber).then((n) => {
+        this.player = n;
+        if (n === 0) {
+          this.roomName = 'Error Joining Room';
+          this.prompt.category = 'Error joining room';
+          this.prompt.word = '';
+        }
+      });
+    });
+    this.router.navigate(['/play', { host: '0', name: this.roomName }]);
   }
 }
