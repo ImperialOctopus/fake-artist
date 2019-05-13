ecf8c0e28d865b42808dfe04121be8d86ebe4c23
Zoey Child <imperialoctopus@gmail.com>
2019-05-13 02:11:19 +0100

    Add reduced size background image

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { isNgTemplate } from '@angular/compiler';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,12 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+(() => {
+  const body = document.body;
+  const img = new Image();
+  img.src = '/assets/background.jpg';
+  img.onload = () => {
+    body.style.backgroundImage = 'url(\'/assets/background.jpg\')';
+  };
+})();
