e603d4078e52bdc533ac7eb0f06437dd2558f199
Zoey Child <imperialoctopus@gmail.com>
2019-05-13 04:41:35 +0100

    Add offline persistence

--- src/app/wordlist.service.ts
+++ src/app/wordlist.service.ts
@@ -1,21 +1,28 @@
 import { Prompt } from './prompt';
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 import * as wordlist from './wordlists/wordlist.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordlistService {
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   async generatePrompt(): Promise<Prompt> {
+
+    const c = await this.db.collection('wordlist').get();
+    c.subscribe(queriedItems => {
+      console.log(queriedItems);
+    });
+    return null;
+  }
+
+  async generatePromptOffline(): Promise<Prompt> {
     const array: Array<object> = wordlist.words;
 
-    const prompt: Prompt = Object.assign(
+    return Object.assign(
       array[Math.floor(Math.random() * array.length)]
     );
-
-    return prompt;
   }
 }
